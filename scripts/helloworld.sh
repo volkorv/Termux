43e772e4345855176f6f19b9feb58f4304b2aabb
volkorv <fy2aj7wj@gmail.com>
2024-12-28 15:34:46 +0000

    Update helloworld.sh

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,10 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
 function log_message {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
 }
+
 log_message "Пакеты Termux обновляются.."
-pkg update -y -qq;pkg upgrade -y -qq;clear;log_message "Пакеты Termux обновляются.."
+pkg update -y -qq
+pkg upgrade -y -qq
+clear
+log_message "Пакеты Termux обновляются.."
+
 log_message "Начинаю установку OpenSSH.."
-pkg install openssh -y -qq;clear log_message "Начинаю установку OpenSSH.."
+pkg install openssh -y -qq
+clear
+log_message "Пакеты Termux обновляются.."
+log_message "Начинаю установку OpenSSH.."
 
 if [ $? -eq 0 ]; then
     log_message "Установка OpenSSH завершена успешно."
