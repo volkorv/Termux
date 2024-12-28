a30cb623b3501ebe54ee84369658da08efee3a5e
volkorv <fy2aj7wj@gmail.com>
2024-12-28 15:36:15 +0000

    Update helloworld.sh

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -3,22 +3,29 @@
 function log_message {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
 }
+read -p "Вы уверены, что хотите запустить скрипт? (y/n): " confirm
 
 log_message "Пакеты Termux обновляются.."
 pkg update -y -qq
 pkg upgrade -y -qq
 clear
-log_message "Пакеты Termux обновляются.."
+
+log_message "Пакеты Termux обновлены."
 
 log_message "Начинаю установку OpenSSH.."
-pkg install openssh -y -qq
-clear
-log_message "Пакеты Termux обновляются.."
-log_message "Начинаю установку OpenSSH.."
+read -p "Вы уверены, что хотите установить OpenSSH? (y/n): " confirm
 
-if [ $? -eq 0 ]; then
-    log_message "Установка OpenSSH завершена успешно."
+if [[ $confirm == [yY] ]]; then
+    pkg install openssh -y -qq
+    clear
+
+    if [ $? -eq 0 ]; then
+        log_message "Установка OpenSSH завершена успешно."
+    else
+        log_message "Ошибка при установке OpenSSH."
+        exit 1
+    fi
 else
-    log_message "Ошибка при установке OpenSSH."
-    exit 1
+    log_message "Установка OpenSSH отменена."
+    exit 0
 fi
