a1ffabdcb9332af25d178401ccac08287f402b6e
volkorv <fy2aj7wj@gmail.com>
2024-12-28 15:25:18 +0000

    Update helloworld.sh

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,1 +1,12 @@
-echo hello world!
+function log_message {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+log_message "Обновляю пакеты Termux.."
+log_message "Начинаю установку OpenSSH..."
+
+if sudo apt-get update -qq && sudo apt-get install -y openssh-server -qq; then
+    log_message "Установка OpenSSH завершена успешно."
+else
+    log_message "Ошибка при установке OpenSSH."
+    exit 1
+fi
