669c1b013913c95010affe1c2089856a76a320ad
volkorv <fy2aj7wj@gmail.com>
2024-12-28 15:32:46 +0000

    Update helloworld.sh

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -2,9 +2,9 @@ function log_message {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
 }
 log_message "Пакеты Termux обновляются.."
-pkg update -y -qq;pkg upgrade -y -qq
+pkg update -y -qq;pkg upgrade -y -qq;clear;log_message "Пакеты Termux обновляются.."
 log_message "Начинаю установку OpenSSH.."
-pkg install openssh -y -qq
+pkg install openssh -y -qq;clear log_message "Начинаю установку OpenSSH.."
 
 if [ $? -eq 0 ]; then
     log_message "Установка OpenSSH завершена успешно."
